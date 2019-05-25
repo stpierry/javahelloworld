5fcc30aca15a955c79d6ce2f80d342d32f832b23
stpierry <yves.stpierre@hotmail.com>
2019-05-24 21:31:06 -0400

    first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
       public static void main (String [] args )
       {
-             System.out.println("Hello World yst");
+             System.out.println("Hello World yst2");
       }
 }
